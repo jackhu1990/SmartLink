72c2ae2f3f8d27aa0bf838dd732c4aab4424888f
jackhu <jackhu1990@gmail.com>
2016-09-10 12:59:14 +0800

    完善安卓版本模块封装.匿名接口.

--- src/android/SmartLink.java
+++ src/android/SmartLink.java
@@ -17,7 +17,19 @@ import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnDismissListener;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SmartLink extends CordovaPlugin implements OnSmartLinkListener{
     //连接控制器
