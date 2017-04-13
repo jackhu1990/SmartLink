badce148ee3319f880d4e1e51945377d794de7ef
jackhu1990 <jackhu1990@gmail.com>
2017-04-13 16:53:38 +0800

    Update SmartLink.js
    stopSmartLinking

--- www/SmartLink.js
+++ www/SmartLink.js
@@ -10,6 +10,9 @@ SmartLink.prototype.getWifiInfo = function (successCallback, errorCallback) {
 SmartLink.prototype.isSmartLinking = function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "SmartLink", "isSmartLinking", []);
 }
+SmartLink.prototype.stopSmartLinking = function (successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "SmartLink", "stopSmartLinking", []);
+}
 
 SmartLink.install = function () {
     if (!window.plugins) {
